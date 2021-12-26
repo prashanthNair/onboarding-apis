62784e9d782a9a7d352639df24a74ae0cb7f7eab
Nandhu <nandubiju99@gmail.com>
2021-12-26 16:10:58 +0530

    update business details

--- app/model/brandUpdateModel.ts
+++ app/model/brandUpdateModel.ts
@@ -1,33 +1,36 @@
 export class BrandUpdateModel {
-    BrandId: string;
-    Status: string;
-    Name: string;
-    Password: string;
-    BrandName: string;
-    Category: string;
-    Country: string;
-    CountryCode: string;
-    CreatedDate: string;
-    Domain: string;
-    EmailId: string;
-    UpdatedAt: string;
-    Mobile: string;
-    RegBusinessName: string;
-    RegisteredType: string;
-    Subscriptions: string;
-    Address:Address;
-    GSTN:string;
-    BankDetails:BankDetails
-    }
-    class Address {
-        Street: string;
-        City: string;
-        State: string;
-        PostalCode: number;
-      }
-class BankDetails{
+  BrandId: string;
+  Status: string;
+  Name: string;
+  Password: string;
+  BrandName: string;
+  Category: string;
+  Country: string;
+  CountryCode: string;
+  CreatedDate: string;
+  Domain: string;
+  EmailId: string;
+  UpdatedAt: string;
+  Mobile: string;
+  RegBusinessName: string;
+  RegisteredType: string;
+  Subscriptions: string;
+  Address: Address;
+  GSTN: string;
+  BankDetails: BankDetails;
+  PAN: String;
+  PANOwnerName: String;
+  BillingName: String;
+}
+class Address {
+  Street: string;
+  City: string;
+  Region: string;
+  PostalCode: number;
+}
+class BankDetails {
   BeneficiaryName: String;
-  BranchIfscCode:string;
-  AccountNumber:string;
-  AccountHolderame:string;
+  BranchIfscCode: string;
+  AccountNumber: string;
+  AccountHolderame: string;
 }
